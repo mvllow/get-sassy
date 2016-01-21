2a381d9b52ee3fa75d34eb0e453a709dc3437844
not <dev.mellow@gmail.com>
2016-01-21 12:08:35 -0600

    clean up

--- README.md
+++ README.md
@@ -1,13 +1,25 @@
 # stylus-flat-ui-colors
 [Flat UI Colors](http://flatuicolors.com) functions, mixins, and variables for stylus and sass
 
+## Getting started
+This library comes in three variants, including css, stylus, and sass.
+
+To get the bare minimum up and running, download `/dist/css/flat-ui-colors.min.css` and reference this file in your project like any other stylesheet.
+
 ## Usage
-Coming soon to a store near you
+You now have access to several classes including the below:
+
+```css
+.color-turquoise { background-color: #1abc9c; }
+.color-text-turquoise { color: #1abc9c; }
+```
+
+For a full list of colors you may choose from, see http://flatuicolors.com
 
 ## Compile
 To compile yourself, download the respective folder within `/src` and run the below command(s).
 
-### Stylus
+#### Stylus
 ```
 $ stylus -w main.styl
 ```
@@ -25,7 +37,7 @@ The newly generated css classes will now prepended with **fui**:
 .fui-color-text-turquoise { color: #1abc9c; }
 ```
 
-### Sass
+#### Sass
 
 ```shell
 $ sass -o css -w styl
--- build.sh
+++ build.sh
@@ -1,90 +1,134 @@
 #!/bin/bash
 clear
 
-version=0.1
+version="0.1"
+package="flat-ui-colors"
+
+color_end="\e[0m"
+color_green="\e[0;32m"
 
 # Check dist file structure
 function checkFiles {
     if [ ! -d dist ]; then
+        echo "creating directory: dist"
         mkdir -p dist
     fi
 
     if [ ! -d dist/css ]; then
+        echo "creating directory: dist/css"
         mkdir -p dist/css
     fi
 
     if [ "$lang" = "stylus" ]; then
         if [ ! -d dist/stylus ]; then
+            echo "creating directory: dist/stylus"
             mkdir -p dist/stylus
         fi
     fi
 
     if [ "$lang" = "sass" ]; then
         if [ ! -d dist/sass ]; then
+            echo "creating directory: dist/sass"
             mkdir -p dist/sass
         fi
     fi
 }
 
-echo
+echo "Flat UI Colors compiler"
+echo "-"
 echo "Please select your language"
-echo "(All options compile to their language and vanilla CSS)"
 echo
-echo "1. Sass (Coming soon)"
-echo "2. Stylus"
-echo
-echo "3. Compile and compress all"
+echo "1. Stylus"
+echo "2. Sass"
 echo
 echo -e "Option #: "
-read lang
+read r_lang
 
-if [ "$lang" = "1" ]; then
+if [ "$r_lang" = "1" ]; then
     clear
-
-    lang="sass"
-
-    sass src/$lang/main.scss dist/css/flat-ui-colors.${min}css
-elif [ "$lang" = "2" ]; then
-    clear
-
     lang="stylus"
+    lang_ext="styl"
+    default_params=""
 
-    echo
     echo -e "Prefix all classes with 'fui-'? (y/N)"
-    read option_isPrefixed
+    read r_is_prefixed
 
-    if [ "$option_isPrefixed" = "y" ]; then
-        isPrefixed="--prefix fui-"
+    if [ "$r_is_prefixed" = "y" ]; then
+        is_prefixed="--prefix fui-"
+        is_prefixed_name="pre."
     fi
 
     echo
     echo -e "Compress output CSS? (y/N)"
-    read option_isCompressed
+    read r_is_compressed
 
-    if [ "$option_isCompressed" = "y" ]; then
-        min="min."
-        isCompressed="-c"
+    if [ "$r_is_compressed" = "y" ]; then
+        is_compressed="-c"
+        is_compressed_name="min."
     fi
 
-    options="$isPrefixed $isCompressed"
+    echo "setting up parameters..."
+    params="$is_prefixed $is_compressed"
+    name="$package.${is_prefixed_name}${is_compressed_name}css"
 
     echo "checking file structure..."
     checkFiles
 
     echo "compiling $lang to css..."
-    stylus $options -o dist/css/flat-ui-colors.${min}css src/$lang/main.styl > /dev/null 2>&1
+    stylus $params -o dist/css/$name src/$lang/main.$lang_ext > /dev/null 2>&1
 
     echo "compiling lib..."
     echo "/*!
      * version: $version
      * mellowdev.com | @sociallymellow
      * github.com/sociallymellow/stylus-flat-ui-colors
-     */" > dist/$lang/flat-ui-colors.styl
+     */" > dist/$lang/$package.$lang_ext
 
-    cat src/$lang/partials/_variables.styl >> dist/$lang/flat-ui-colors.styl
-    cat src/$lang/partials/_functions.styl >> dist/$lang/flat-ui-colors.styl
-    cat src/$lang/partials/_helpers.styl >> dist/$lang/flat-ui-colors.styl
-    cat src/$lang/partials/_mixins.styl >> dist/$lang/flat-ui-colors.styl
+    cat src/$lang/partials/_variables.$lang_ext >> dist/$lang/$package.$lang_ext
+    cat src/$lang/partials/_functions.$lang_ext >> dist/$lang/$package.$lang_ext
+    cat src/$lang/partials/_helpers.$lang_ext >> dist/$lang/$package.$lang_ext
+    cat src/$lang/partials/_mixins.$lang_ext >> dist/$lang/$package.$lang_ext
 
-    echo -e "\e[0;32msuccess\e[0m"
+    echo -e "${color_green}success${color_end}"
+    echo
+
+elif [ "$r_lang" = "2" ]; then
+    clear
+    lang="sass"
+    lang_ext="scss"
+    default_params=""
+
+    echo
+    echo -e "Compress output CSS? (y/N)"
+    read r_is_compressed
+
+    if [ "$r_is_compressed" = "y" ]; then
+        is_compressed="--style compressed"
+        is_compressed_name="min."
+    fi
+
+    echo "setting up parameters..."
+    params="$is_compressed"
+    name="$package.${is_compressed_name}css"
+
+    echo "checking file structure..."
+    checkFiles
+
+    echo "compiling $lang to css..."
+    sass src/$lang/main.$lang_ext dist/css/$name $params
+
+    echo "compiling lib..."
+    echo "/*!
+     * version: $version
+     * mellowdev.com | @sociallymellow
+     * github.com/sociallymellow/stylus-flat-ui-colors
+     */" > dist/$lang/$package.$lang_ext
+
+    cat src/$lang/partials/_variables.$lang_ext >> dist/$lang/$package.$lang_ext
+    cat src/$lang/partials/_functions.$lang_ext >> dist/$lang/$package.$lang_ext
+    cat src/$lang/partials/_helpers.$lang_ext >> dist/$lang/$package.$lang_ext
+    cat src/$lang/partials/_mixins.$lang_ext >> dist/$lang/$package.$lang_ext
+
+    echo -e "${color_green}success${color_end}"
+    echo
 fi
